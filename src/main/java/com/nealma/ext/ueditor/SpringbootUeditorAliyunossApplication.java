ffa095a13135aba83054a77b5397f050ba49a53a
neal.ma <neal.ma.sh@gmail.com>
2017-12-19 15:01:55 +0800

    init

---
+++ src/main/java/com/nealma/ext/ueditor/SpringbootUeditorAliyunossApplication.java
@@ -0,0 +1,12 @@
+package com.nealma.ext.ueditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUeditorAliyunossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootUeditorAliyunossApplication.class, args);
+	}
+}
